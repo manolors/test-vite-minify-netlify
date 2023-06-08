d6cbb6a93d4480449afe2d9eeb27226fbb283338
manolors <manolo.rs2005@gmail.com>
2023-06-08 17:10:36 +0100

    Función hello world

---
+++ netlify/functions/hello-world/hello-world.js
@@ -0,0 +1,15 @@
+// Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
+const handler = async (event) => {
+  try {
+    console.log(event);
+    const subject = event.queryStringParameters.name || 'World'
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: `Hello ${subject}` }),
+    }
+  } catch (error) {
+    return { statusCode: 500, body: error.toString() }
+  }
+}
+
+module.exports = { handler }
