d49946352953cb6ab74a4c080e3667c99290d06e
manolors <manolo.rs2005@gmail.com>
2023-06-08 18:52:14 +0100

    add more data

--- netlify/functions/suma/suma.js
+++ netlify/functions/suma/suma.js
@@ -1,13 +1,13 @@
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
 const handler = async (event) => {
   try {
-    const subject = event.queryStringParameters.name || 'World'
+////////////////////////////////////////////
+
+
+////////////////////////////////////////////
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
+      body: JSON.stringify({ message: `Hello ${subject}`, data: "🥑", otracosa:{ lala : 2} }),
     }
   } catch (error) {
     return { statusCode: 500, body: error.toString() }
